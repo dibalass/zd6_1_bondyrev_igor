5bf13461e5c680c38d7ea448a8024cec77319092
dibalass <123352922+dibalass@users.noreply.github.com>
2023-06-21 12:56:57 +0500

    Add files via upload

---
+++ zd6_1_bondyrev/zd6_1_bondyrev/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zd6_1_bondyrev
+{
+    public partial class MainPage :CarouselPage
+    {
+        public MainPage (string login, string password)
+        {
+            InitializeComponent();
+        }
+    }
+}
